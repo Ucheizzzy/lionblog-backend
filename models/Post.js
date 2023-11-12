460bc86b6d61286eed98effbe249a0e4123ddcfd
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-11-12 22:11:23 +0100

    working on comment and category models and schemas

---
+++ models/Post.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose'
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: String,
+    image: {
+      type: String,
+      required: true,
+    },
+    claps: {
+      type: String,
+      default: 0,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    shares: {
+      type: Number,
+      default: 0,
+    },
+    postViews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    schedulePublished: {
+      type: Date,
+      default: null,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    disLikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comments',
+      },
+    ],
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('Post', PostSchema)
