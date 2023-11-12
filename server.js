5c7b5a35b815159076e611ea700992b5acfe4bd3
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-11-12 20:40:17 +0100

    working on the user schema and model

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 const app = express()
+import morgan from 'morgan'
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 app.get('/', (req, res) => {
   res.send('Hello')
 })
